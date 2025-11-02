6b0277e8c3dbe5d5bd4655f953e6c89e7c547968
makhanek <makhanek@infocom.dev>
2025-11-02 15:09:35 +0300

    chng base

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'lemana-pro-quiz',
   server: {
     port: 9000,
   },
