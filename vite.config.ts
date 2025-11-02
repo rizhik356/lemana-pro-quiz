96ad865a3e8a4d2944b95f0dad1b0c7f09a14884
makhanek <makhanek@infocom.dev>
2025-11-02 15:03:20 +0300

    add github build

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'lemana-pro-quiz',
   server: {
-    port: 9000
-  }
+    port: 9000,
+  },
 })
